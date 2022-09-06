d4091332fe6c0a72b72b630f655b3013228da03a
204G1A0559 <94514847+204G1A0559@users.noreply.github.com>
2022-09-06 11:58:28 +0530

    K.MOULIKA CREATED FCFS.C

---
+++ OSLAB/FCFS.C
@@ -0,0 +1,6 @@
+/*   Name Of the Candidate: K.MOULIKA
+   Roll No: 204G1A0559
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
